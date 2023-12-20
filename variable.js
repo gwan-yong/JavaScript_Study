cc61f623f45f9b64507d334da8196357ab90da2e
gwan-yong <128782708+gwan-yong@users.noreply.github.com>
2023-12-20 15:03:56 +0900

    변수 선언

---
+++ variable.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+console.log(symbol1 === symbol2); // false
+const gsymbol1 = Symbol.for('id');
+const gsymbol2 = Symbol.for('id');
+console.log(gsymbol1 === gsymbol2); //ture
+console.log(`id : ${symbol1.description}`); 
